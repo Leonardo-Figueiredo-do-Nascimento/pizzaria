db1075b63acdcbd5d192268f9a21f275e4d8d2b5
LEONARDO FIGUEIREDO DO NASCIMENTO <kriptonascimento@hotmail.com>
2024-10-13 20:09:04 -0300

    Pagina de tamanhos, sabores e arquivo com sabores e preços desenvolvidos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { MenuComponent } from "./pages/menu/menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, MenuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   title = 'pizzaria';
--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { PizzaService } from '../../services/pizza.service';
+import { SizeComponent } from '../size/size.component';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [SizeComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
-  
+
+  pizzas: any;
+
+  selectedPizza: string | null = null;
+
+  onPizzaSelect(sabor: string): void {
+    this.selectedPizza = sabor;
+  }
+
+  constructor(private service: PizzaService){
+    this.service.getPizzaMenu().subscribe(data=>{
+      this.pizzas = data;
+      console.log(this.pizzas)
+    })
+  }
 }
