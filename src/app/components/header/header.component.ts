8fa0433e1becf59f9e8057a8acc7774abc8ecf4d
LEONARDO FIGUEIREDO DO NASCIMENTO <kriptonascimento@hotmail.com>
2024-10-21 15:24:55 -0300

    Atualização do Header desenvolvida

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,33 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PedidoService } from '../../services/pedido.service';
+import { Pedido } from '../../models/pedido';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgIf,RouterModule],
+  imports: [NgIf, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-    pizzaIcon = "./assets/pizza-icon.png";
-    deliveryIcon = "./assets/delivery-icon.png";
+export class HeaderComponent implements OnInit {
 
-    pedidoCount: number = 0;
+  pedido: Pedido | undefined; 
+  pizzaIcon = "./assets/pizza-icon.png";
+  deliveryIcon = "./assets/delivery-icon.png";
+  pedidoCount: number = 0;
 
-    addPedido(): void{
-      this.pedidoCount++;
-    }
+  constructor(private pedidoService: PedidoService) { }
+
+  ngOnInit(): void {
+    this.pedidoService.getPedidoObservable().subscribe(pedido => {
+      this.pedido = pedido; 
+      this.updatePedidoCount(); 
+    });
+  }
+
+  updatePedidoCount(): void {
+    this.pedidoCount = this.pedido?.pizzas ? this.pedido.pizzas.length : 0; 
+  }
 }
--- src/app/services/pedido.service.ts
+++ src/app/services/pedido.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Pedido } from '../models/pedido';
 import { Pizza } from '../models/pizza';
 
@@ -13,10 +14,21 @@ export class PedidoService {
     precoTotal: 0.00 
   };
 
+  private pedidoSubject = new BehaviorSubject<Pedido>(this.pedido);
+
   constructor() { }
 
-  adicionarPizza(pizza:Pizza,preço:number){
-    this.pedido.pizzas.push(pizza)
+  adicionarPizza(pizza: Pizza, preço: number) {
+    this.pedido.pizzas.push(pizza);
     this.pedido.precoTotal += preço;
+    this.pedidoSubject.next(this.pedido);
+  }
+
+  getPedido(): Pedido {
+    return this.pedido;
+  }
+
+  getPedidoObservable() { 
+    return this.pedidoSubject.asObservable();
   }
 }
